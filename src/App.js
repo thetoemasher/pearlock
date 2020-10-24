169796e82a472a589ccb0ab06dd12300a3a4d553
Cody <tomeschcody@gmail.com>
2020-10-23 19:19:12 -0600

    login without loading

--- src/App.js
+++ src/App.js
@@ -9,15 +9,13 @@ import GameStart from './components/GameStart'
 function App() {
   const [user, setUser] = useState('')
   return (
-    <div className='row bg-secondary' style={{width: '100vw', height: '100vh'}}>
-      <div className='col'>
+    <div className='row bg-secondary d-flex flex-row justify-content-center align-items-center' style={{width: '100vw', height: '100vh'}}>
         <Switch>
           <Route path='/' exact render={(props) => <Login setUser={setUser} {...props}/>}/>
           <Route path='/:username/:game_id/:game_name/submit' exact render={(props) => <Submit setUser={setUser} user={user} {...props}/>}/>
           <Route path='/:user' exact render={(props) => <UserPage setUser={setUser} user={user} {...props}/>}/>
           <Route path='/:username/game/:game_id/:game_name/play' render={(props) => <GameStart setUser={setUser} user={user} {...props}/>}/>
         </Switch>
-      </div>
     </div>
   );
 }
